e3f4f36390e047ebe3c26a4835e54519a7ea40ce
Chigusa Izumi <chigusa.f124@gmail.com>
2020-05-14 19:19:06 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by 泉 千種 on 2020/05/14.
+//  Copyright © 2020 Izumi zu-mi-. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
